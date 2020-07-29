de8a004e8a030bc38e0265d098faefb16f9b1f9a
Marcin Bartminski <mbdjj@o2.pl>
2020-07-29 07:51:22 +0200

    Initial Commit

---
+++ School Tasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  School Tasks
+//
+//  Created by Marcin Bartminski on 29/07/2020.
+//  Copyright © 2020 Marcin Bartminski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
