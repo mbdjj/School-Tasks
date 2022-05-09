cfe5c2e4e440132ede0b9af7594a48c5cc5c468f
Marcin Bartmiński <mbdjj@o2.pl>
2022-05-09 18:24:17 +0200

    Initial Commit

---
+++ School Tasks/School_TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  School_TasksApp.swift
+//  School Tasks
+//
+//  Created by Marcin Bartminski on 09/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct School_TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
